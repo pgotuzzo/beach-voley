42fe86becd143f5a34bc5695581ca983fb84843d
Francisco López Destain <francisco.lopez@fdvsolutions.com>
2017-10-07 16:34:50 -0300

    Crear random utils

---
+++ util/RandomNumber.hpp
@@ -0,0 +1,20 @@
+#ifndef BEACH_VOLEY_RANDOMNUMBER_H
+#define BEACH_VOLEY_RANDOMNUMBER_H
+
+/**
+ * Creates a random integer in the interval [minValue, maxValue).
+ *
+ * @param minValue : the min value in the interval.
+ * @param maxValue : the min value in the interval.
+ * @return the random integer.
+ */
+int getRandomInt(int minValue, int maxValue);
+
+/**
+ * Creates a random double in the interval [0, maxValue).
+ *
+ * @return the random double.
+ */
+double getRandomDouble();
+
+#endif //BEACH_VOLEY_RANDOMNUMBER_H
